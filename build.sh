ed09fcc4582dbb0353812f07c9b39168cbe36d32
Dmytro Teteruk <dmytro.teteruk@globallogic.com>
2013-03-14 16:30:11 +0200

    Fix build.sh in compiling different platform

--- build.sh
+++ build.sh
@@ -3,10 +3,14 @@ platform=$1
 if [ -z $platform ];
 then
   gyp --depth . -D library=static_library -G output_dir=bin -D platform=desktop -G config=desktop --generator-output=bin/desktop wd.gyp
+  cd bin/desktop
   make
+  cd ../..
   gyp --depth . -D library=static_library -G output_dir=bin -D platform=flip -G config=flip --generator-output=bin/flip wd.gyp
+  cd bin/flip
   make
 else
   gyp --depth . -D library=static_library -G output_dir=bin -D platform=$platform -G config=$platform --generator-output=bin/$platform wd.gyp
+  cd bin/$platform
   make
 fi
