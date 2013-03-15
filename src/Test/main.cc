8307c8155b456d2e1bd20949bca49ce6b2535f45
Andriy Prystupa <andriy.prystupa@globallogic.com>
2013-03-15 10:17:33 +0200

    added simple native application

--- src/Test/main.cc
+++ src/Test/main.cc
@@ -6,6 +6,63 @@
 #include <QtCore/QObject>
 #include <QtCore/QFutureWatcher>
 #include <QtCore/QtConcurrentRun>
+//#include <QDebug>
+
+#include <QtGui/QWidget>
+#include <QtGui/QLineEdit>
+#include <QtGui/QPushButton>
+
+#include <QtWebKit/QtWebKit>
+
+
+class MainWindow : public QWidget
+ {
+     Q_OBJECT
+
+ public:
+     MainWindow();
+     virtual ~MainWindow();
+
+ private slots:
+     void testPushButton();
+
+ private:
+     QLineEdit *textEdit;
+     QPushButton *pushButton;
+     QWebView *webView;
+ };
+
+MainWindow::MainWindow()
+{
+    textEdit = new QLineEdit(this);
+    textEdit->move(10, 10);
+    textEdit->setAccessibleName("InputText");
+
+    pushButton = new QPushButton(this);
+    pushButton->setText("Push me");
+    pushButton->setAccessibleName("PushButton");
+    pushButton->move(10, 60);
+
+    webView = new QWebView(this);
+    webView->move(10, 100);
+    webView->setWindowTitle(QString("WD"));
+
+    this->resize(1024, 800);
+    this->setWindowTitle("WD_native");
+
+    connect(pushButton, SIGNAL(clicked()), this, SLOT(testPushButton()));
+}
+
+MainWindow::~MainWindow()
+{
+
+}
+
+void MainWindow::testPushButton()
+{
+    qDebug() << "\n\n!!!!!!!!!!!! Test PushButton !!!!!!!!!!!!!!\n\n";
+    textEdit->setText("It's alive ;-)");
+}
 
 int main(int argc, char *argv[])
 {
@@ -26,6 +83,10 @@ int main(int argc, char *argv[])
     QWebSettings::globalSettings()->setOfflineStoragePath("./web/html5");
     QWebSettings::globalSettings()->setOfflineWebApplicationCachePath("./web/html5");
 
+    // start hybridTest window
+    MainWindow mainWindow;
+    mainWindow.show();
+
     QFutureWatcher<int> watcher;
     QObject::connect(&watcher, SIGNAL(finished()), qApp, SLOT(quit()));
     QFuture<int> future = QtConcurrent::run(main_server, argc, argv);
@@ -33,3 +94,6 @@ int main(int argc, char *argv[])
 
     return app.exec();
 }
+
+
+#include "moc_main.cc"
