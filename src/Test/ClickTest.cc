37c74e4a5a7daeab6177d8127fee755b488f2245
Andrii Moroz <Andrii.Moroz@globallogic.com>
2013-04-17 17:56:10 +0300

    Added widgest for native tests

---
+++ src/Test/ClickTest.cc
@@ -0,0 +1,39 @@
+/* ClickTest.cc
+  */
+
+#include "ClickTest.h"
+
+static const QString WINDOW_TITLE = "CLick Test Window";
+
+ClickTestWidget::ClickTestWidget()
+{
+    connect(&(this->normalBtn), SIGNAL(clicked()), this, SLOT(OnNormalBtnClick()));
+    connect(&(this->checkBox), SIGNAL(clicked()), this, SLOT(OnCheckBoxClick()));
+    connect(&(this->btnOnScroolArea), SIGNAL(clicked()), this, SLOT(OnBtnOnScrollClick()));
+
+    this->normalBtn.setObjectName("pushBtn");
+    this->checkBox.setObjectName("checkBox");
+    this->btnOnScroolArea.setObjectName("btnOnScroll");
+    this->scrollArea.setViewport(&btnOnScroolArea);
+
+    this->setWindowTitle("ClickTest");
+}
+
+ClickTestWidget::~ClickTestWidget()
+{}
+
+void ClickTestWidget::OnNormalBtnClick()
+{
+    this->setWindowTitle(WINDOW_TITLE);
+}
+
+void ClickTestWidget::OnCheckBoxClick()
+{
+    if (checkBox.isChecked())
+        this->setWindowTitle(WINDOW_TITLE);
+}
+
+void ClickTestWidget::OnBtnOnScrollClick()
+{
+    this->setWindowTitle(WINDOW_TITLE);
+}
