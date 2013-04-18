edf50bc4c6cbc2c481bcfb4d1a7e8db698122a36
Andrii Moroz <Andrii.Moroz@globallogic.com>
2013-04-18 14:11:25 +0300

    Fixed qt5 issues

--- src/Test/WindowTest.h
+++ src/Test/WindowTest.h
@@ -1,7 +1,13 @@
  /* WindowTest.h
    */
 
+#include <QtCore/qglobal.h>
+
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 0, 0))
+#include <QtWidgets/QWidget>
+#else
 #include <QtGui/QWidget>
+#endif
 
 class WindowTestWidget : public QWidget
 {
