6e4f88544d59f2d5cfd367aca3f46d340689170e
Andrii Boichuk <Andrii.Boichuk@globallogic.com>
2013-04-23 17:34:55 +0300

    Add positioning of widgets in main window

--- src/Test/ClickTest.cc
+++ src/Test/ClickTest.cc
@@ -7,10 +7,21 @@ static const QString WINDOW_TITLE = "CLick Test Window";
 
 ClickTestWidget::ClickTestWidget()
 {
+    this->resize(400, 400);
+
     this->normalBtn = new QPushButton(this);
+    this->normalBtn->setText("Change Title");
+    this->normalBtn->move(10, 10);
+
     this->checkBox = new QCheckBox(this);
+    this->checkBox->setText("Change Title");
+    this->checkBox->move(40, 40);
+
     this->scrollArea = new QScrollArea(this);
+    this->scrollArea->move(80, 80);
     this->btnOnScroolArea = new QPushButton(this);
+    this->btnOnScroolArea->move(80, 80);
+    this->scrollArea->setWidget(btnOnScroolArea);
 
     connect(this->normalBtn, SIGNAL(clicked()), this, SLOT(OnNormalBtnClick()));
     connect(this->checkBox, SIGNAL(clicked()), this, SLOT(OnCheckBoxClick()));
@@ -19,7 +30,6 @@ ClickTestWidget::ClickTestWidget()
     this->normalBtn->setObjectName("pushBtn");
     this->checkBox->setObjectName("checkBox");
     this->btnOnScroolArea->setObjectName("btnOnScroll");
-    this->scrollArea->setViewport(btnOnScroolArea);
 
     this->setWindowTitle("ClickTest");
 }
