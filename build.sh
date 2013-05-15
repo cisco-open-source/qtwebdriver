e6c65cd275f228ad12275930338375cd6924cce3
Dmytro Teteruk <dmytro.teteruk@globallogic.com>
2013-05-15 18:08:09 +0300

    Quick fix for build system merge issue

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ base_output_gen=`dirname ${output_gen}`
 current_dir=`pwd`
 if [ -z $output_gen ];
 then
-  output_gen=$current_dir
+  output_gen='.'
   platform="desktop"
 fi
 
@@ -30,19 +30,19 @@ do
   for mode in $modes
   do
     cd $current_dir
-    gyp --depth . -G output_dir=. -D platform=$platform -D mode=$mode -D ROOT_PATH=${base_output_gen} --generator-output=${output_gen}/$platform/$mode wd.gyp
+    gyp --depth . -G output_dir=. -D platform=$platform -D mode=$mode -D ROOT_PATH=${base_output_gen} --generator-output=${output_gen}/out/$platform/$mode wd.gyp
     [ $? -ne 0 ] && exit 1
-    cd ${output_gen}/$platform/$mode
+    cd ${output_gen}/out/$platform/$mode
     [ $? -ne 0 ] && echo "**** ERROR: Can't access to ${output_gen}/$platform/$mode" && exit 1
     make
     [ $? -ne 0 ] && exit 1
     mkdir -p ${output_gen}/bin/$platform/$mode/
     [ $? -ne 0 ] && echo "**** ERROR: Can't create ${output_gen}/bin/$platform/$mode" && exit 1
-    cp -f ${output_gen}/$platform/$mode/Default/libWebDriver.a ${output_gen}/bin/$platform/$mode/ 2>/dev/null
-    cp -f ${output_gen}/$platform/$mode/Default/lib.target/libWebDriver.so ${output_gen}/bin/$platform/$mode/ 2>/dev/null
-    if [ -f ${output_gen}/$platform/$mode/Default/WebDriver ]
+    cp -f ${output_gen}/out/$platform/$mode/Default/libWebDriver.a ${output_gen}/bin/$platform/$mode/ 2>/dev/null
+    cp -f ${output_gen}/out/$platform/$mode/Default/lib.target/libWebDriver.so ${output_gen}/bin/$platform/$mode/ 2>/dev/null
+    if [ -f ${output_gen}/out/$platform/$mode/Default/WebDriver ]
     then
-      cp -f ${output_gen}/$platform/$mode/Default/WebDriver ${output_gen}/bin/$platform/$mode/ 2>/dev/null
+      cp -f ${output_gen}/out/$platform/$mode/Default/WebDriver ${output_gen}/bin/$platform/$mode/ 2>/dev/null
     fi
   done
 done
