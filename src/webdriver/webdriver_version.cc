ae1b8efd0cd4641104dbedcff5087b67690d8e04
Nataliia Koval <nataliia.koval@globallogic.com>
2013-06-21 10:21:40 +0300

    Changed the version number

--- src/webdriver/webdriver_version.cc
+++ src/webdriver/webdriver_version.cc
@@ -1,6 +1,6 @@
 namespace webdriver {
 extern const char kProductName[] = "WebDriver-cisco-cmt";
-extern const char kVersionNumber[] = "0.2.0";
+extern const char kVersionNumber[] = "1.0.0";
 extern const char kBuildTime[] = __TIME__;
 extern const char kBuildDate[] = __DATE__;
 extern const char kLastChanges[] = "4e6d397080-dirty";
