74157c47ed81b92bd9394dce4bcf0275830fbd6f
Nataliia Koval <nataliia.koval@globallogic.com>
2013-07-11 11:30:24 +0300

    fixed location webdriver_version.cc

--- generate_wdversion.py
+++ generate_wdversion.py
@@ -1,6 +1,6 @@
 import subprocess as sp
 
-filename = "./src/webdriver/wdversion.cc"
+filename = "./src/webdriver/webdriver_version.cc"
 p = sp.Popen(["git", "describe" , "--abbrev=10", "--dirty", "--always"], stdin=sp.PIPE, stdout=sp.PIPE)
 
 data = p.stdout.readline()
@@ -8,7 +8,7 @@ data = p.stdout.readline()
 versionfile = open (filename, 'w')
 versionfile.write("namespace webdriver {\n")
 versionfile.write("extern const char kProductName[] = \"WebDriver-cisco-cmt\";\n")
-versionfile.write("extern const char kVersionNumber[] = \"0.2.0\";\n")
+versionfile.write("extern const char kVersionNumber[] = \"1.0.0\";\n")
 versionfile.write("extern const char kBuildTime[] = __TIME__;\n")
 versionfile.write("extern const char kBuildDate[] = __DATE__;\n")
 versionfile.write("extern const char kLastChanges[] = \"" + data.strip() + "\";\n")
