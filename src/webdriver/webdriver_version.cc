7c3e582a4942e8ad36568bb446fe17df34b14d51
Nataliia Koval <nataliia.koval@globallogic.com>
2013-07-12 13:22:13 +0300

    removed autogenerated file from repo

--- src/webdriver/webdriver_version.cc
+++
@@ -1,7 +0,0 @@
-namespace webdriver {
-extern const char kProductName[] = "WebDriver-cisco-cmt";
-extern const char kVersionNumber[] = "1.0.0";
-extern const char kBuildTime[] = __TIME__;
-extern const char kBuildDate[] = __DATE__;
-extern const char kLastChanges[] = "4e6d397080-dirty";
-}
