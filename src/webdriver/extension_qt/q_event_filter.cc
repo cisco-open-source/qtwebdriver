34407a21b5f5f97e16ec7c6c822f5b47c03d075d
Nataliia Koval <nataliia.koval@globallogic.com>
2013-07-17 17:37:37 +0300

    add logs for debug

--- src/webdriver/extension_qt/q_event_filter.cc
+++ src/webdriver/extension_qt/q_event_filter.cc
@@ -9,9 +9,15 @@ QRepaintEventFilter::QRepaintEventFilter(QObject *parent) :
 QRepaintEventFilter::~QRepaintEventFilter() {}
 
 bool QRepaintEventFilter::eventFilter(QObject *pobject, QEvent *pevent) {
+    if (pevent->type()== QEvent::Resize) {
+        webdriver::GlobalLogger::Log(webdriver::kInfoLogLevel, "***** Resize ********** in QRepaintEventFilter::eventFilter() ****");
+    }
     if (pevent->type()== QEvent::Paint) {
-        webdriver::GlobalLogger::Log(webdriver::kInfoLogLevel, "*************** in QRepaintEventFilter::eventFilter() ****");
+        webdriver::GlobalLogger::Log(webdriver::kInfoLogLevel, "***** Paint ********** in QRepaintEventFilter::eventFilter() ****");
         emit repainted();
+    }
+    if (pevent->type()== QEvent::Show) {
+        webdriver::GlobalLogger::Log(webdriver::kInfoLogLevel, "***** Show ********** in QRepaintEventFilter::eventFilter() ****");
     }
     return false;
 }
