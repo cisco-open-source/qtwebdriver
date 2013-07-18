27388bb850884eb21b3d3f431b09ae992b9e7634
Nataliia Koval <nataliia.koval@globallogic.com>
2013-07-18 11:17:32 +0300

    delete temp logs for debuging

--- src/webdriver/extension_qt/q_event_filter.cc
+++ src/webdriver/extension_qt/q_event_filter.cc
@@ -1,5 +1,4 @@
 #include "q_event_filter.h"
-#include "webdriver_logging.h"
 
 QRepaintEventFilter::QRepaintEventFilter(QObject *parent) :
     QObject(parent)
@@ -9,21 +8,12 @@ QRepaintEventFilter::QRepaintEventFilter(QObject *parent) :
 QRepaintEventFilter::~QRepaintEventFilter() {}
 
 bool QRepaintEventFilter::eventFilter(QObject *pobject, QEvent *pevent) {
-    if (pevent->type()== QEvent::Resize) {
-        webdriver::GlobalLogger::Log(webdriver::kInfoLogLevel, "***** Resize ********** in QRepaintEventFilter::eventFilter() ****");
-    }
     if (pevent->type()== QEvent::Paint) {
-        webdriver::GlobalLogger::Log(webdriver::kInfoLogLevel, "***** Paint ********** in QRepaintEventFilter::eventFilter() ****");
         emit repainted();
-    }
-    if (pevent->type()== QEvent::Show) {
-        webdriver::GlobalLogger::Log(webdriver::kInfoLogLevel, "***** Show ********** in QRepaintEventFilter::eventFilter() ****");
     }
     return false;
 }
 
 void QCheckPagePaint::pagePainted() {
     is_painting = true;
-    //emit painted();
-    webdriver::GlobalLogger::Log(webdriver::kInfoLogLevel, "******************* in QCheckPagePaint::pagePainted() ****");
 }
