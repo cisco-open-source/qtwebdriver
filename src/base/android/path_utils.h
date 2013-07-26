a350273fd5f357dd32e3eb518fc8e4e47f8a3274
Dmytro Teteruk <dmytro.teteruk@globallogic.com>
2013-07-26 14:15:06 +0300

    Support Android build (library) in gyp

---
+++ src/base/android/path_utils.h
@@ -0,0 +1,39 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_ANDROID_PATH_UTILS_H_
+#define BASE_ANDROID_PATH_UTILS_H_
+
+#include <jni.h>
+#include <string>
+
+namespace base {
+namespace android {
+
+// Return the absolute path to the data directory of the current application.
+// This method is dedicated for base_paths_android.c, Using
+// PathService::Get(base::DIR_ANDROID_APP_DATA, ...) gets the data dir.
+std::string GetDataDirectory();
+
+// Return the absolute path to the cache directory. This method is dedicated for
+// base_paths_android.c, Using PathService::Get(base::DIR_CACHE, ...) gets the
+// cache dir.
+std::string GetCacheDirectory();
+
+// Returns the path to the public downloads directory.
+std::string GetDownloadsDirectory();
+
+// Returns the path to the native JNI libraries via
+// ApplicationInfo.nativeLibraryDir on the Java side.
+std::string GetNativeLibraryDirectory();
+
+// Returns the absolute path to the external storage directory.
+std::string GetExternalStorageDirectory();
+
+bool RegisterPathUtils(JNIEnv* env);
+
+}  // namespace android
+}  // namespace base
+
+#endif  // BASE_ANDROID_PATH_UTILS_H_
