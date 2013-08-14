bbe67fe843191746eb8d9ea0993dd219d437997a
Vasyl Vavrychuk <vvavrychuk@gmail.com>
2013-08-14 18:25:16 +0300

    extracted visualizer commands to separate file

--- inc/extension_qt/web_view_executor.h
+++ inc/extension_qt/web_view_executor.h
@@ -5,7 +5,6 @@
 #include "webdriver_logging.h"
 
 #include <QtCore/QtGlobal>
-#include <QtXml/QDomDocument>
 
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 0, 0))
 #include <QtWebKitWidgets/QWebView>
@@ -101,44 +100,6 @@ public slots:
 
 private:
     JSLogger jslogger;
-};
-
-class QWebViewVisualizerSourceCommand : public QObject
-{
-    Q_OBJECT
-
-public:
-    QWebViewVisualizerSourceCommand(QWebViewCmdExecutor* executor, Session* session, QWebView* view);
-
-    void Execute(std::string* source, Error** error);
-
-private:
-    static QSharedPointer<QDomDocument> ParseXml(const QString& input, Error** error);
-    static void UnescapeXml(QString& input);
-
-    void AssemblePage(QDomElement element) const;
-    void AssembleLink(QDomElement element) const;
-    void AssembleImg(QDomElement element) const;
-    void AssembleStyle(QDomElement element) const;
-    void AssembleStyle(QDomAttr attribute) const;
-    QString AssembleStyle(const QString& value) const;
-    void RemoveScripts(QDomElement element) const;
-
-    QString AbsoluteUrl(const QString& url) const;
-    void Download(const QString& url, QByteArray* buffer, QString* contentType) const;
-    QString DownloadAndEncode(const QString& url) const;
-
-    static QString trimmed(const QString& str, const QString& symbols);
-
-private Q_SLOTS:
-    void DownloadFinished();
-
-private:
-    static const char DATA_PROTOCOL[];
-
-    QWebViewCmdExecutor* executor_;
-    Session* session_;
-    QWebView* view_;
 };
 
 class QWebViewCmdExecutorCreator : public ViewCmdExecutorCreator  {
