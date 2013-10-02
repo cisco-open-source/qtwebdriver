1078cdde97e98c89de6395810fb6c9ca453f5443
Mykola Tryshnivskyy <mykola.tryshnivskyy@globallogic.com>
2013-10-02 10:09:31 +0300

    Move QT_DIR init to toolchain_builder.sh

--- toolchain_builder.sh
+++ toolchain_builder.sh
@@ -1,15 +1,42 @@
 #!/bin/bash
 
 BCM_TOOLCAIN_DIR=/opt/softs/compilers/others/Broadcom/BCM_LNX_1_0_0
+BCM_QT_DIR=/home/jenkins/jobs/BUILD_MVM_All/workspace/build/TAMIAS/WEBKITQT48/cisco-4.8-cmc-openmha-qt4.8.4.2-wk116000.9_d_WEBKITQT48_MAIN_MHA_Int/BCM_LNX_1_0_2/release
+
+
 CHANALD_TOOLCAIN_DIR=/opt/softs/toolchains/CANALD_KAONCO1200C_BCM7356_LNUX_01/v0.5_REL.8/platform_cfg/linux/compiler/mips4k_gcc_x86_linux_22
+CHANALD_QT_DIR=/home/jenkins/jobs/BUILD_MVM_All/workspace/build/TAMIAS/WEBKITQT48/cisco-4.8-cmc-openmha-qt4.8.4.2-wk116000.9_d_WEBKITQT48_MAIN_MHA_Int/BCM_LNX_1_0_2/release
+
+
 NEWCO_TOOLCHAIN_DIR=/opt/softs/toolchains/NEWCO_HUMAXHDR1002S_BCM7356_LNUX_01/I.18.01.03.01_Evolution_MOR_Release6_Integration_ioctl_hijack/platform_cfg/linux/compiler/mips4k_gcc_x86_linux_11
+NEWCO_QT_DIR=/home/jenkins/jobs/BUILD_MVM_All/workspace/build/TAMIAS/WEBKITQT48/cisco-4.8-cmc-openmha-qt4.8.4.2-wk116000.9_d_WEBKITQT48_MAIN_MHA_Int/BCM_LNX_1_0_2/release
+
+
 REF_TOOLCHAIN_DIR=/opt/softs/toolchains/REF_BCM97425_BCM7425_LNUX_01/M6.0.67_6.1_ioctl_hijack/platform_cfg/linux/compiler/mips4k_gcc_x86_linux_12
-ST40_CC14_0_1_TOOLCHAIN_DIR=/opt/softs/compilers/st/st40/LINUX_4.2.4.jan0711/STLinux-2.3/devkit/sh4_uclibc
-ST40_CC15_0_0_TOOLCHAIN_DIR=/opt/softs/compilers2/st/st40/LINUX_4.2.4_0.8.0/STLinux-2.4/devkit/sh4_uclibc
+REF_QT_DIR=/home/jenkins/jobs/BUILD_MVM_All/workspace/build/TAMIAS/WEBKITQT48/cisco-4.8-cmc-openmha-qt4.8.4.2-wk116000.9_d_WEBKITQT48_MAIN_MHA_Int/BCM_LNX_1_0_2/release
+
+
+ST40_CC14_0_1_TOOLCHAIN_DIR=/opt/softs/toolchains/ST40_CC14_0_1/STLinux-2.3-gcc4.2.4-uclibc_05
+ST40_CC14_0_1_QT_DIR=/home/jenkins/jobs/BUILD_MVM_All/workspace/build/TAMIAS/WEBKITQT48/cisco-4.8-cmc-0.9.REL_WEBKITQT48_MAIN_MHA_Int/ST40_CC14_0_1/release
+
+
+ST40_CC15_0_0_TOOLCHAIN_DIR=/opt/softs/toolchains/ST40_CC15_0_0/STLinux-2.4-gcc4.5.3-uclibc_03
+ST40_CC15_0_0_QT_DIR=/home/jenkins/jobs/BUILD_MVM_All/workspace/build/TAMIAS/WEBKITQT48/cisco-4.8-cmc-openmha-qt4.8.4.3-wk116000.10_b_WEBKITQT48_MAIN_MHA_Int/ST40_CC15_0_0/debug
+
+
 UPCH_TOOLCHAIN_DIR=/opt/softs/toolchains/UPCH_SAMVGW_INTELGRV_LNUX_01/M16.0_126_7.1/platform_cfg/linux/compiler/i686_gcc_x86_linux_01/i686-linux-elf
+UPCH_QT_DIR=/home/jenkins/jobs/BUILD_MVM_All/workspace/build/TAMIAS/WEBKITQT48/UPCH_SAMVGW_INTELGRV_LNUX_01/release
+
+
 VOO_TOOLCHAIN_DIR=/opt/softs/toolchains/VOO_CISCOISB8K_BCM7241_LNUX_01/M6.0.75_M3_8.2/platform_cfg/linux/compiler/mips4k_gcc_x86_linux_22
+VOO_QT_DIR=/home/jenkins/jobs/BUILD_MVM_All/workspace/build/TAMIAS/WEBKITQT48/cisco-4.8-cmc-openmha-qt4.8.4.2-wk116000.9_d_WEBKITQT48_MAIN_MHA_Int/BCM_LNX_1_0_2/release
+
+
 ARMV7_TOOLCHAIN_DIR=/opt/softs/compilers/st/arm/LINUX_4.6.3_112/armv7
+
+
 ARM_ANGSTROM_TOOLCHAIN_DIR=/opt/softs/compilers/st/arm/LINUX_4.6.3_112/arm-angstrom
+ARM_ANGSTROM_QT_DIR=/opt/android/Qt5.1/5.1.0/android_armv7
 
 
 
@@ -28,56 +55,72 @@ function SetPlatformVariable
 			export CC=${BCM_TOOLCAIN_DIR}/bin/mips-linux-uclibc-gcc
 			export CXX=${BCM_TOOLCAIN_DIR}/bin/mips-linux-uclibc-g++
 			export LINK=${BCM_TOOLCAIN_DIR}/bin/mips-linux-uclibc-g++
+			export QT_DIR=${BCM_QT_DIR}
 			;;
 		BCMEL)
 			export CC=${BCM_TOOLCAIN_DIR}/bin/mipsel-linux-uclibc-gcc
 			export CXX=${BCM_TOOLCAIN_DIR}/bin/mipsel-linux-uclibc-g++
 			export LINK=${BCM_TOOLCAIN_DIR}/bin/mipsel-linux-uclibc-g++
+			export QT_DIR=${BCM_QT_DIR}
 			;;
 		CHANALD)
 			export CC=${CHANALD_TOOLCAIN_DIR}/bin/mipsel-linux-uclibc-gcc
 			export CXX=${CHANALD_TOOLCAIN_DIR}/bin/mipsel-linux-uclibc-g++
 			export LINK=${CHANALD_TOOLCAIN_DIR}/bin/mipsel-linux-uclibc-g++
+			export QT_DIR=${CHANALD_QT_DIR}
 			;;
 		NEWCO)
 			export CC=${NEWCO_TOOLCHAIN_DIR}/bin/mipsel-linux-uclibc-gcc
 			export CXX=${NEWCO_TOOLCHAIN_DIR}/bin/mipsel-linux-uclibc-g++
 			export LINK=${NEWCO_TOOLCHAIN_DIR}/bin/mipsel-linux-uclibc-g++
+			export QT_DIR=${NEWCO_QT_DIR}
 			;;
 		REF)
 			export CC=${REF_TOOLCHAIN_DIR}/bin/mipsel-linux-uclibc-gcc
 			export CXX=${REF_TOOLCHAIN_DIR}/bin/mipsel-linux-uclibc-g++
 			export LINK=${REF_TOOLCHAIN_DIR}/bin/mipsel-linux-uclibc-g++
+			export QT_DIR=${REF_QT_DIR}
 			;;
+
 		ST40_CC14_0_1)
 			export CC=${ST40_CC14_0_1_TOOLCHAIN_DIR}/bin/sh4-linux-uclibc-gcc
 			export CXX=${ST40_CC14_0_1_TOOLCHAIN_DIR}/bin/sh4-linux-uclibc-g++
 			export LINK=${ST40_CC14_0_1_TOOLCHAIN_DIR}/bin/sh4-linux-uclibc-g++
+			export QT_DIR=${ST40_CC14_0_1_QT_DIR}
 			;;
+
 		ST40_CC15_0_0)
 			export CC=${ST40_CC15_0_0_TOOLCHAIN_DIR}/bin/sh4-linux-uclibc-gcc
 			export CXX=${ST40_CC15_0_0_TOOLCHAIN_DIR}/bin/sh4-linux-uclibc-g++
 			export LINK=${ST40_CC15_0_0_TOOLCHAIN_DIR}/bin/sh4-linux-uclibc-g++
+			export QT_DIR=${ST40_CC15_0_0_QT_DIR}
 			;;
+
 		UPCH)
 			export CC=${UPCH_TOOLCHAIN_DIR}/bin/i686-cm-linux-gcc
 			export CXX=${UPCH_TOOLCHAIN_DIR}/bin/i686-cm-linux-g++
 			export LINK=${UPCH_TOOLCHAIN_DIR}/bin/i686-cm-linux-g++
+			export QT_DIR=${UPCH_QT_DIR}
 			;;
+
 		VOO)
 			export CC=${VOO_TOOLCHAIN_DIR}/bin/mipsel-linux-uclibc-gcc
 			export CXX=${VOO_TOOLCHAIN_DIR}/bin/mipsel-linux-uclibc-g++
 			export LINK=${VOO_TOOLCHAIN_DIR}/bin/mipsel-linux-uclibc-g++
+			export QT_DIR=${VOO_QT_DIR}
 			;;
 		ARMV7)
 			export CC=${ARMV7_TOOLCHAIN_DIR}/bin/armv7-linux-gcc
 			export CXX=${ARMV7_TOOLCHAIN_DIR}/bin/armv7-linux-g++
 			export LINK=${ARMV7_TOOLCHAIN_DIR}/bin/armv7-linux-g++
+			export QT_DIR=${ARMV7_QT_DIR}
 			;;
+
 		ARM_ANGSTROM)
 			export CC=${ARM_ANGSTROM_TOOLCHAIN_DIR}/bin/arm-angstrom-linux-gnueabi-gcc
 			export CXX=${ARM_ANGSTROM_TOOLCHAIN_DIR}/bin/arm-angstrom-linux-gnueabi-g++
 			export LINK=${ARM_ANGSTROM_TOOLCHAIN_DIR}/bin/arm-angstrom-linux-gnueabi-g++
+			export QT_DIR=${ARM_ANGSTROM_QT_DIR}
 			;;
 
 	esac
