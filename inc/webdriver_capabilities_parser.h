6ef11a1a299bd363366ce9ba32df44d45a3d0b49
Volodymyr Zherebchuk <Volodymyr.Zherebchuk@globallogic.com>
2014-10-15 18:21:31 +0300

    recovered native license headers
    Change-Id: Ieff5b815163ef42323ed9f5cb1e5459a8b2c7e6a
    Signed-off-by: Volodymyr Zherebchuk <Volodymyr.Zherebchuk@globallogic.com>

--- inc/webdriver_capabilities_parser.h
+++ inc/webdriver_capabilities_parser.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 /*! \page page_hybrid_capabilities Hybrid WD capabilities
 
 <h2>Start window capabilities</h2>
--- inc/webdriver_session.h
+++ inc/webdriver_session.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 /*! \page page_sessions Sessions
 
 Session class represents object that handles all information related to WD session.
