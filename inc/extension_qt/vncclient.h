b821b038263634cf54bda05e3181b5fa9d2e94bb
Volodymyr Zherebchuk <Volodymyr.Zherebchuk@globallogic.com>
2014-10-15 14:13:42 +0300

    added CISCO license file header
    Change-Id: I795f7b8e9105af7f78b309b727f710e72292e192
    Signed-off-by: Volodymyr Zherebchuk <Volodymyr.Zherebchuk@globallogic.com>

--- inc/extension_qt/vncclient.h
+++ inc/extension_qt/vncclient.h
@@ -1,3 +1,22 @@
+/****************************************************************************
+**
+** Copyright © 1992-2014 Cisco and/or its affiliates. All rights reserved.
+** All rights reserved.
+** 
+** $CISCO_BEGIN_LICENSE:LGPL$
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** $CISCO_END_LICENSE$
+**
+****************************************************************************/
+
 /*! \file vncclient.h
     \page vncclient
   vncclient - WebDriver module, which allows connect to VNC server.
