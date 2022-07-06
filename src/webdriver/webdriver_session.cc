6c3a8611b5fe7180241ea1fbbb4dce741b4984b1
shahargk8 <86890863+shahargk8@users.noreply.github.com>
2022-07-06 15:17:29 +0300

    dangeling pointer: Delete iterator in iteration
    https://en.cppreference.com/w/cpp/container/map/erase
    "References and iterators to the erased elements are invalidated. Other references and iterators are not affected. '

--- src/webdriver/webdriver_session.cc
+++ src/webdriver/webdriver_session.cc
@@ -501,9 +501,9 @@ void Session::UpdateViews(const std::set<ViewId>& views) {
     ViewsMap::iterator it;
     ViewId vi;
 
-    for (it = views_.begin(); it != views_.end(); ++it) {
+    for (it = views_.begin(); it != views_.end();) {
         vi = ViewId(it->first);
-		
+	 ++it;	
         if (vi.is_valid() && 0 == views.count(vi)) {
             // invalidate handle
             RemoveView(vi);
